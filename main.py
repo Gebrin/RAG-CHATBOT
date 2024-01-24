1553416840735219247f2386b78ba2333c7b8c8d
Gebrin <gebrinbiju123@gmail.com>
2024-01-24 12:37:58 +0530

    continuing hte project

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import streamlit as st
+from PyPDF2 import PdfReader
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
-# Press the green button in the gutter to run the script.
+def handle_userinput():
+    pass
+def main():
+    st.set_page_config(page_title="Query Answering Application")
+    st.header("Welcome to Query Answering Application")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf
+                raw_text = get_pdf_text(pdf_docs)
+                # proces pdf
+                # Rag Mixtreal AI
+                # chatgpt openai query
+                # conversation chain
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
